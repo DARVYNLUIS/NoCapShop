b558aeba8b5a24bd95d953087f9741374b18bd19
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-12-10 11:49:06 -0400

    feat: ✨ crear ProductosDto para la transferencia de datos.

---
+++ app/src/main/java/com/edu/darvyn/nocap/data/remote/dto/ProductosDto.kt
@@ -0,0 +1,16 @@
+package com.edu.darvyn.nocap.data.remote.dto
+
+data class ProductosDto (
+    val productoId : Int?,
+    val productoNombre: String,
+    val productoDescripcion: String,
+    val productoImagne: String?,
+    val fechaCreacionProducto: String?,
+    val precioProductoVenta: Double,
+    val stocks: Int,
+    val categoriaId: Int,
+    val marcaId: Int,
+    val activo: Boolean,
+    val tamaños: List<String>,
+    val colores: List<String>
+)

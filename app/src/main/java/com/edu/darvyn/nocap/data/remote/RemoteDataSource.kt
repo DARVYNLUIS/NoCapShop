dd2903ec3be676cb205e46d3a683ae5286ac803f
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-12-10 11:50:41 -0400

    feat: ✨ implementar RemoteDataSource para llamadas a la API.

---
+++ app/src/main/java/com/edu/darvyn/nocap/data/remote/RemoteDataSource.kt
@@ -0,0 +1,35 @@
+package com.edu.darvyn.nocap.data.remote
+
+import com.edu.darvyn.nocap.data.remote.api.NoCapApi
+import com.edu.darvyn.nocap.data.remote.dto.CategoriaDto
+import com.edu.darvyn.nocap.data.remote.dto.MarcasDto
+import com.edu.darvyn.nocap.data.remote.dto.ProductosDto
+import com.edu.darvyn.nocap.data.remote.dto.RequestLogin
+import com.edu.darvyn.nocap.data.remote.dto.UsuarioDto
+import com.edu.darvyn.nocap.domain.model.Producto
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val noCapApi: NoCapApi
+) {
+    //Categoria
+    suspend fun observeAllCategorias() = noCapApi.observeAllCategorias()
+    suspend fun saveCategoria(categoriaDto: CategoriaDto) = noCapApi.saveCategoria(categoriaDto)
+    suspend fun editCategoria(id: Int, categoriaDto: CategoriaDto) = noCapApi.editCategoria(id, categoriaDto)
+    suspend fun eliminarCategoria(id: Int) = noCapApi.eliminarCategoria(id)
+
+    //Marcas
+    suspend fun observeAllMarcas() = noCapApi.observeAllMarcas()
+    suspend fun saveMarcas(marcasDto: MarcasDto) = noCapApi.saveMarca(marcasDto)
+    suspend fun editMarca(id: Int, marcasDto: MarcasDto) = noCapApi.editMarca(id,marcasDto)
+    suspend fun eliminarMarca (id: Int) = noCapApi.deleteMarca(id)
+
+    //Usuario
+    suspend fun iniciarSesion(request : RequestLogin) = noCapApi.iniciarSesion(request)
+    suspend fun crearSesion(usuarioDto: UsuarioDto) = noCapApi.crearUsuario(usuarioDto)
+
+    //Producto
+    suspend fun observeAllProductos() = noCapApi.observeAllProductos()
+    suspend fun saveProducto(producto: ProductosDto) = noCapApi.saveProducto(producto)
+    suspend fun deleteProducto(id: Int) = noCapApi.deleteProducto(id)
+}

284e67fdc40208e606a5654caf307e587f1982df
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-12-10 12:42:43 -0400

    feat: ✨ implementar ObserveProductoViewModel con lógica de agregar al carrito

---
+++ app/src/main/java/com/edu/darvyn/nocap/presetantion/catalago/observeProducto/ProductoDetalleViewModel.kt
@@ -0,0 +1,166 @@
+package com.edu.darvyn.nocap.presetantion.catalago.observeProducto
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.edu.darvyn.nocap.data.remote.Resource
+import com.edu.darvyn.nocap.domain.useCase.useCaseCarritos.AgregarProductoUseCase
+import com.edu.darvyn.nocap.domain.useCase.useCaseProducto.ObserveProductoByIdUseCase
+import com.edu.darvyn.nocap.domain.useCase.useCaseUsuarios.Login.ObserveUserLoggedUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ObserveProductoViewModel @Inject constructor(
+    private val observeProductoByIdUseCase: ObserveProductoByIdUseCase,
+    private val agregarProductoAlCarritoUseCase: AgregarProductoUseCase,
+    private val observeUserLoggedUseCase : ObserveUserLoggedUseCase
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(ProductoDetalleUiState())
+    val state: StateFlow<ProductoDetalleUiState> = _state.asStateFlow()
+
+    init {
+        onEvent(ProductoDetalleUiEvent.LoadUsuario)
+    }
+
+    private fun loadProducto(productoId: Int) {
+        _state.update { it.copy(isLoading = true, error = null) }
+
+        viewModelScope.launch {
+            try {
+                val productoEncontrado = observeProductoByIdUseCase(productoId)
+
+                if (productoEncontrado.productoId == null) {
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            error = "Producto no encontrado"
+                        )
+                    }
+                    return@launch
+                }
+
+                _state.update {
+                    it.copy(
+                        producto = productoEncontrado,
+                        productoId = productoEncontrado.productoId,
+                        isLoading = false
+                    )
+                }
+
+            } catch (e: Exception) {
+                _state.update {
+                    it.copy(
+                        isLoading = false,
+                        error = e.message ?: "Error inesperado"
+                    )
+                }
+            }
+        }
+    }
+
+    fun onEvent(event: ProductoDetalleUiEvent) {
+        when (event) {
+            is ProductoDetalleUiEvent.SelectTalla -> {
+                _state.value = _state.value.copy(selectedTalla = event.talla)
+            }
+            is ProductoDetalleUiEvent.SelectColor -> {
+                _state.value = _state.value.copy(selectedColor = event.color)
+            }
+            is ProductoDetalleUiEvent.ChangeCantidad -> {
+                _state.value = _state.value.copy(cantidad = event.cantidad)
+            }
+            is ProductoDetalleUiEvent.AgregarAlCarrito -> {
+                agregarAlCarrito()
+            }
+
+           is  ProductoDetalleUiEvent.LoadProducto -> {
+               loadProducto(event.productoId)
+           }
+
+            ProductoDetalleUiEvent.LoadUsuario -> getUsuario()
+        }
+    }
+
+    fun getUsuario(){
+        viewModelScope.launch {
+            val user = observeUserLoggedUseCase()
+            _state.update {
+                it.copy(
+                    isLoading =  user != null,
+                    user = user
+                )
+            }
+        }
+    }
+
+    private fun agregarAlCarrito() {
+        val currentState = _state.value
+        val producto = currentState.producto
+        val talla = currentState.selectedTalla
+        val color = currentState.selectedColor
+        val cantidad = currentState.cantidad
+
+        // Validaciones de UI
+        if (producto == null) {
+            _state.update { it.copy(error = "Producto no cargado") }
+            return
+        }
+
+        if (talla.isNullOrBlank()) {
+            _state.update { it.copy(error = "Selecciona una talla") }
+            return
+        }
+
+        if (color.isNullOrBlank()) {
+            _state.update { it.copy(error = "Selecciona un color") }
+            return
+        }
+
+        viewModelScope.launch {
+            _state.update { it.copy(isLoading = true) }
+
+            val result = agregarProductoAlCarritoUseCase(
+                usuarioId = _state.value.user?.usuarioId ?: 0 ,
+                productoId = producto.productoId!!,
+                cantidad = cantidad,
+                precio = producto.precioVenta,
+                color = color,
+                talla = talla
+            )
+
+            when (result) {
+                is Resource.Success -> {
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            mensajeExito = result.data.toString() ?: "Producto agregado al carrito",
+                            selectedTalla = null,
+                            selectedColor = null,
+                            cantidad = 1,
+                            error = null,
+                        )
+                    }
+                }
+
+                is Resource.Error -> {
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            error = result.message ?: "Error al agregar al carrito"
+                        )
+                    }
+                }
+
+                is Resource.Loading -> {
+                }
+            }
+        }
+    }
+}
+

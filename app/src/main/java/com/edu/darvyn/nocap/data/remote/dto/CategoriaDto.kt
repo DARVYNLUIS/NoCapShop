9b559ef7ed73aee254b6a120e9aa3c3291970216
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-12-10 11:47:15 -0400

    feat: ✨ crear CategoriaDto para la transferencia de datos.

---
+++ app/src/main/java/com/edu/darvyn/nocap/data/remote/dto/CategoriaDto.kt
@@ -0,0 +1,8 @@
+package com.edu.darvyn.nocap.data.remote.dto
+
+data class CategoriaDto (
+    val categoriaId: Int?,
+    val nombre: String,
+    val descripcion: String,
+    val activo : Boolean
+)

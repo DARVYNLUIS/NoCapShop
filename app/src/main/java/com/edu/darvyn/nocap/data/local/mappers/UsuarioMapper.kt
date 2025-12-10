77641934e4425fd1ae1c1dba170e18a61bf024c0
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-12-10 11:43:21 -0400

    feat: ✨ crear mappers de Usuario.

---
+++ app/src/main/java/com/edu/darvyn/nocap/data/local/mappers/UsuarioMapper.kt
@@ -0,0 +1,30 @@
+package com.edu.darvyn.nocap.data.local.mappers
+
+import com.edu.darvyn.nocap.data.local.entities.UsuarioEntity
+import com.edu.darvyn.nocap.data.remote.dto.UsuarioDto
+import com.edu.darvyn.nocap.domain.model.Usuario
+
+fun UsuarioEntity.toDomain(): Usuario? = Usuario(
+    usuarioId = usuarioId,
+    nombres = nombres,
+    email = email,
+    password = null,
+    confirmPassword = null,
+    rol = rol
+)
+
+
+fun UsuarioDto.dtoToEntity() : UsuarioEntity = UsuarioEntity(
+    usuarioId = usuarioId,
+    nombres = nombres,
+    email = correo,
+    rol = rolId ?: 2
+)
+
+fun Usuario.domainToDto() : UsuarioDto = UsuarioDto(
+    usuarioId = usuarioId,
+    nombres = nombres,
+    correo = email,
+    contraseña = password,
+    rolId = rol
+)

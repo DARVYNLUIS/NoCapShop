2c95ed66feeee271b6bc3073d9048f3d88fd6e3b
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-12-10 12:01:59 -0400

    chore: 🔧 configurar AppModule para inyección de DB y DAOs

---
+++ app/src/main/java/com/edu/darvyn/nocap/di/AppModule.kt
@@ -0,0 +1,56 @@
+package com.edu.darvyn.nocap.di
+
+import android.content.Context
+import androidx.room.Room
+import com.edu.darvyn.nocap.data.local.database.NoCapDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideNoCapDb(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            NoCapDatabase::class.java,
+            "NoCap.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideCategoria(noCapDb: NoCapDatabase) = noCapDb.categoriaDao()
+
+    @Provides
+    @Singleton
+    fun provideMarcas(noCapDb: NoCapDatabase) = noCapDb.marcaDao()
+
+    @Provides
+    @Singleton
+    fun provideUsuario(noCapDb: NoCapDatabase) = noCapDb.usuarioDao()
+    @Provides
+    @Singleton
+    fun provideProducto(noCapDb: NoCapDatabase) = noCapDb.productoDao()
+
+    @Provides
+    @Singleton
+    fun provideCarrito(noCapDb: NoCapDatabase) = noCapDb.carritoDao()
+
+    @Provides
+    @Singleton
+    fun provideOrdenCompra(noCapDb: NoCapDatabase) = noCapDb.ordenCompraDao()
+
+    @Provides
+    @Singleton
+    fun providePedido(noCapDb: NoCapDatabase) = noCapDb.pedidoDao()
+
+    @Provides
+    @Singleton
+    fun providePagos(noCapDb: NoCapDatabase) = noCapDb.pagosDao()
+}
